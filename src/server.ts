e6ff47fe078287f7a68cb69a7f1d24ae1e75f1b9
Sergey <chernetsky@gmail.com>
2021-07-12 11:57:16 +0800

    Env file

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,21 @@
+import * as mongoose from 'mongoose';
+import 'dotenv/config';
 import App from './app';
 import PostsController from './posts/posts.controller';
 
+
+const {
+  MONGO_USER,
+  MONGO_PASSWORD,
+  MONGO_PATH,
+  PORT
+} = process.env;
+// mongoose.connect(`mongodb://${MONGO_USER}:${MONGO_PASSWORD}${MONGO_PATH}`);
+
+
+
 const app = new App([
   new PostsController()
-], 5000);
+], PORT);
 
 app.listen();
