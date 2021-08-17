939ca67c2674e4b67b02fec90815c1fd3169d789
Sergey <chernetsky@gmail.com>
2021-08-17 11:28:39 +0800

    Edded linter

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,16 @@
 import * as express from 'express';
 import * as bodyParser from 'body-parser';
+import * as mongoose from 'mongoose';
+import Controller from './interfaces/controlles.interface'
 
 class App {
   public app: express.Application;
   public port: number;
 
-  constructor(controllers, port) {
+  constructor(controllers: Controller[]) {
     this.app = express();
-    this.port = port;
 
+    this.connectToTheDatabase();
     this.initializeMiddlewares();
     this.initializeControllers(controllers);
   }
@@ -17,15 +19,26 @@ class App {
     this.app.use(bodyParser.json());
   }
 
-  private initializeControllers(controllers) {
+  private initializeControllers(controllers: Controller[]) {
     controllers.forEach(controller => {
       this.app.use('/', controller.router)
     });
   }
 
+  private connectToTheDatabase() {
+    const {
+      MONGO_USER,
+      MONGO_PASSWORD,
+      MONGO_PATH,
+      PORT
+    } = process.env;
+
+    mongoose.connect(`mongodb+srv://${MONGO_USER}:${MONGO_PASSWORD}${MONGO_PATH}`, { useNewUrlParser: true, useUnifiedTopology: true });
+  }
+
   public listen() {
-    this.app.listen(this.port, () => {
-      console.log(`App listening on the port ${this.port}`);
+    this.app.listen(process.env.PORT, () => {
+      console.log(`App listening on the port ${process.env.PORT}`);
     })
   }
 }
--- src/server.ts
+++ src/server.ts
@@ -1,23 +1,10 @@
-import * as mongoose from 'mongoose';
 import 'dotenv/config';
 import App from './app';
 import PostsController from './posts/posts.controller';
+import validateEnv from './utils/validateEnv';
 
+validateEnv();
 
-const {
-  MONGO_USER,
-  MONGO_PASSWORD,
-  MONGO_PATH,
-  PORT
-} = process.env;
-
-console.log(`mongodb+srv://${MONGO_USER}:${MONGO_PASSWORD}${MONGO_PATH}`)
-mongoose.connect(`mongodb+srv://${MONGO_USER}:${MONGO_PASSWORD}${MONGO_PATH}`);
-
-
-
-const app = new App([
-  new PostsController()
-], PORT);
+const app = new App([new PostsController()]);
 
 app.listen();
